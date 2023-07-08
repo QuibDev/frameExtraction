05d1217ea0c3754d4f2f2d60b89ba5b263a0d87a
Quibdev <89295808+QuibDev@users.noreply.github.com>
2023-07-08 12:30:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def extract_frames(video_path, output_folder):
+def extract_frames(video_path, output_folder, num_frames):
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -12,8 +12,17 @@ def extract_frames(video_path, output_folder):
     # Get total number of frames in the video
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    print("\nDetected Video Footage at {} ...".format(video_path))
+
+    # Calculate the step size to evenly extract frames
+    step_size = max(total_frames // num_frames, 1)
+
     # Extract frames
-    for frame_num in range(total_frames):
+    frame_num = 1
+    while frame_num <= total_frames:
+        # Set the frame position
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_num-1)
+
         # Read the frame
         success, frame = video.read()
 
@@ -24,14 +33,33 @@ def extract_frames(video_path, output_folder):
         frame_path = os.path.join(output_folder, f"frame_{frame_num}.jpg")
         cv2.imwrite(frame_path, frame)
 
+        frame_num += step_size
+
     # Release the video file
     video.release()
+    print("Frames saved in folder at {} ...".format(output_folder))
 
-# Path to the input video file
-video_path = "path_to_your_video.avi"
+# Path to the main folder containing videos and subfolders
+videos_folder = "C://Users/akash/Videos/1201"
 
-# Path to the output folder
-output_folder = "frames"
+# Number of frames to extract
+num_frames = 10
 
-# Call the function to extract frames
-extract_frames(video_path, output_folder)
+# Recursive function to search for videos in subfolders
+def search_videos(folder_path):
+    # Iterate through all items in the folder
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
+
+        if os.path.isdir(item_path):
+            # Recursively search videos in subfolders
+            search_videos(item_path)
+        elif item.endswith(".avi"):
+            # Extract frames for the video
+            video_name = os.path.splitext(item)[0]
+            output_folder = os.path.join(folder_path, "Frames", video_name)
+            extract_frames(item_path, output_folder, num_frames)
+
+
+# Start searching videos in the main folder and subfolders
+search_videos(videos_folder)
